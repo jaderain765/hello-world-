854c478484513f38a038444dfddeff45093ebb15
MASTER <MASTER@MY_COM>
2022-03-30 06:53:16 +0900

    initial commit

---
+++ src/StringApp.java
@@ -0,0 +1,11 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");//String
+		System.out.println("Hello \nworld");
+		System.out.println("Hello \"world\"");
+	}
+
+}
